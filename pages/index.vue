57d34231cf5c1f064a25e7d5b6e5ea022086624f
rainu <rainu@raysha.de>
2023-08-02 13:42:10 +0200

    check if openai token is available

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,25 @@
 <template>
+  <v-container>
+    <v-alert
+        type="error"
+        :title="$t('validation.openai.missing-token.title')"
+        :text="$t('validation.openai.missing-token.text')"
+    />
+  </v-container>
 </template>
+
+<script>
+import {mapState} from "pinia";
+import {useSettingsStore} from "~/store/settings";
+
+export default {
+  computed: {
+    ...mapState(useSettingsStore, ['openai']),
+  },
+  mounted() {
+    if(this.openai.token) {
+      this.$router.push('/tagesschau');
+    }
+  }
+}
+</script>
